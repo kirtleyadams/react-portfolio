b0c135d6c223d36962420cceec81dae792405452
Kirtley Adams <92392080+kirtleyadams@users.noreply.github.com>
2023-06-16 20:55:52 -0400

    nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
             <nav className="py-2 navTop">
   <div className="container d-flex flex-wrap">
     <ul className="nav ms-auto">
-      <li className="nav-item"><a href="#" className="nav-link link-dark px-2">HOME</a></li>
+      <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Home</a></li>
       <li className="nav-item"><a href="#about" className="nav-link link-dark px-2">ABOUT</a></li>
       <li className="nav-item"><a href="#contact" className="nav-link link-dark px-2">CONTACT</a></li>
     </ul>
